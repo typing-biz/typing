e319eb98eb8bd42092650db2b126728e0fb00bba
“NargizaRysbek” <r.nakosya@gmail.com>
2021-08-05 11:57:16 +0600

    added header and home layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.scss'
+import {Routes} from '../../routes'
 
 function Header() {
-    return (
-        <div>
-            Header
-        </div>
-    )
+	return (
+		<header className='header'>
+			<div className='container'>
+
+				<Link className="link" to={Routes.home}>
+					<div className='header__logo'>ANJ Typing</div>
+				</Link>
+
+				<nav>
+					<Link to={Routes.testing}>Тестирование</Link>
+					<Link to={Routes.rating}>Рейтинг</Link>
+				</nav>
+
+				{/* <Select defaultValue='EN | RU' style={{ width: 120 }}>
+					<Option value='EN'>EN</Option>
+					<Option value='RU'>RU</Option>
+				</Select>
+
+				<div>
+					<Link to={routes.login}>
+						<Button type='text'>Log In</Button>
+					</Link>
+					<Link to={routes.sign_up}>
+						<CustomButton primary>Sign Up</CustomButton>
+					</Link>
+				</div> */}
+			</div>
+		</header>
+	)
 }
 
 export default Header
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import './Home.scss'
 
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+	return (
+		<div className='home'>
+			<div className="container">
+				<h2>Ratatype</h2>
+				<div className='home__main-block'>
+					<h1>Печатай <br/> быстрее</h1>
+					<div>
+						<p>
+							Нучись быстро печатать с клавиатурным тренажером{' '}
+							<br />
+							Ratatype. А уроки слепой печати помогут тебе <br />
+							использовать все 10 пальцев.
+						</p>
+						<img src='' alt='' />
+					</div>
+					<div className="home__btns">
+						<button className="home__start-btn">Начать печатать</button>
+						<button className="home__rating-btn">Рейтинг</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Home
