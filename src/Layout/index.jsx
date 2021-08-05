7b9fd73716c2e2ade65e92ed314ce9fa3de81cce
jasminaisaeva <jasminaisaeva2001@gmail.com>
2021-08-05 17:02:40 +0600

    added sign up page

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -3,10 +3,11 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Routes from '../routes'
 
+
 function Layout() {
 	return (
-		<>
-			<Header />
+		<>   
+		    <Header />
 			<Routes />
 			<Footer />
 		</>
