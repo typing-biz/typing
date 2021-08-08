e3ba729b7016ac322b0b87377aed7e389d879075
jasminaisaeva <jasminaisaeva2001@gmail.com>
2021-08-08 15:00:06 +0600

    test

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,16 +18,16 @@ function Header() {
 	const history = useHistory()
 	const dispatch = useDispatch()
 
-	switch(switchSelect) {
-		case 'footer':
-			 alert('test')
-			break;
-		case 'out':
-			dispatch(logOut())
-			break;
-		default:
-			setSwitchSelect('profile')
-	}
+	// switch(switchSelect) {
+	// 	case 'footer':
+	// 		 alert('test')
+	// 		break;
+	// 	case 'out':
+	// 		dispatch(logOut())
+	// 		break;
+	// 	default:
+	// 		setSwitchSelect('profile')
+	// }
 
 	return (
 		<header className='header'>
